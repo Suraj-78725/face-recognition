57d17e5ba0df0a9f3cf021f2b1d2dfc4f62bdf29
Suraj-78725 <surajbansode2004@gmail.com>
2023-11-07 22:24:05 +0530

    face file is added in this project which is main file of the project

--- Face_Recognition8956/face.py
+++ Face_Recognition8956/face.py
@@ -3,5 +3,44 @@ import face_recognition
 import pickle
 import os
 
+# importing student images
+folderPath = 'images'
+PathList = os.listdir(folderPath)
+print(PathList)
+imgList = []
+studentIds = []
+# import thing mode images into a list
+for path in PathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+    # print(path)
+    # print(os.path.splitext(path)[0])
 
-folderPath="Images"
+print(studentIds)
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)
+        if len(encode) > 0:
+            encodeList.append(encode[0])
+        else:
+            # Handle the case when no faces are detected in the image
+            encodeList.append(None)
+        encodeList.append(encode)
+    return encodeList
+
+
+print("Encodeing Startingg ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithids = [encodeListKnown,studentIds]
+# print(encodeListKnown)
+print("Encodeing ending ...")
+
+
+file = open("EncodeFile","wb")
+pickle.dump(encodeListKnownWithids,file)
+file.close()
+print("file saved")
